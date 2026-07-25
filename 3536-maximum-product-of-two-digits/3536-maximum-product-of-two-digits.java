5600acf28da2ff56880cbdb16b318922a7550969
Kaveri Bhamre <152000849+KaveriBhamre@users.noreply.github.com>
2026-07-25 18:48:27 +0530

    Time: 3 ms (19.58%), Space: 42.6 MB (58.74%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,13 +1,6 @@
 class Solution {
     public int maxProduct(int n) {
-        if(n < 100){
-            int d1 = n % 10;
-            n /= 10;
-            int d2 = n % 10;
-
-            return d1 * d2;
-        }
-
+        
         ArrayList<Integer> list = new ArrayList<>();
 
         while(n > 0) {
@@ -17,15 +10,11 @@ class Solution {
         }
 
         list.sort(null);
-        int count = 0;
-        int product = 1;
-
+        
         int last = list.get(list.size() - 1);
         int secondLast = list.get(list.size() - 2);
 
-        product = last * secondLast;
-
-        return product;
+        return last * secondLast;
 
     }
 }
