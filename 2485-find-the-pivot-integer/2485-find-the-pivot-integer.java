a2a27e8b85b886e214e98117d2facccfe418423d
Kaveri Bhamre <152000849+KaveriBhamre@users.noreply.github.com>
2026-08-07 16:09:06 +0530

    Time: 1 ms (71.18%), Space: 42.3 MB (75.22%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,9 +1,6 @@
 class Solution {
     public int pivotInteger(int n) {
-        int totalSum = 0;
-        for(int i = 1; i <= n; i++){
-            totalSum += i;
-        }
+        int totalSum = n * (n+1) / 2;
         
         int leftSum = 0;
         for(int i = 1; i <= n; i++){
