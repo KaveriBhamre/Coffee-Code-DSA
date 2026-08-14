66ec53b7d3ea4dd422df2acceb8f6ab6dfe9ce90
Kaveri Bhamre <152000849+KaveriBhamre@users.noreply.github.com>
2026-08-14 16:22:21 +0530

    Time: 1 ms (98.36%), Space: 42.9 MB (15.71%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,10 +1,11 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        return String.valueOf(Math.abs(n))
-                        .chars()
-                        .map(Character :: getNumericValue)
-                        .sum();
-
-        
+      int sum = 0;
+      while( n>0){
+          int digit = n %10;
+          sum += digit;
+          n /=10;
+      }
+      return sum;  
     }
 }
